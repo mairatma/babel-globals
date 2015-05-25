3924ccfc23602877c4511f2796b0f3e98fd7e90c
Maira Bello <maira.araujo@liferay.com>
2015-05-25 16:40:04 -0300

    Converts indentation to use spaces

--- index.js
+++ index.js
@@ -6,99 +6,99 @@ var BabelGlobalsFormatter = require('babel-globals-module-formatter');
 var Concat = require('concat-with-sourcemaps');
 
 function addUsedHelpers(concat, results) {
-	var usedHelpers = getUsedExternalHelpers(results);
-	concat.add('helpers.js', babel.buildExternalHelpers(usedHelpers));
+  var usedHelpers = getUsedExternalHelpers(results);
+  concat.add('helpers.js', babel.buildExternalHelpers(usedHelpers));
 }
 
 function compileToGlobals(files, options) {
-	dependencies = {};
-	visited = {};
+  dependencies = {};
+  visited = {};
 
-	options = normalizeOptions(options);
-	var results = babelDeps(files, options.babelOptions);
-	var orderedResults = sortModules(results);
+  options = normalizeOptions(options);
+  var results = babelDeps(files, options.babelOptions);
+  var orderedResults = sortModules(results);
 
-	var concat = new Concat(true, options.bundleFileName, '\n');
-	initializeGlobalVar(concat, options);
-	addUsedHelpers(concat, results);
-	for (var i = 0; i < orderedResults.length; i++) {
-		concat.add(orderedResults[i].path, orderedResults[i].babel.code, orderedResults[i].babel.map);
-	}
-	return concat;
+  var concat = new Concat(true, options.bundleFileName, '\n');
+  initializeGlobalVar(concat, options);
+  addUsedHelpers(concat, results);
+  for (var i = 0; i < orderedResults.length; i++) {
+    concat.add(orderedResults[i].path, orderedResults[i].babel.code, orderedResults[i].babel.map);
+  }
+  return concat;
 }
 
 function getUsedExternalHelpers(results) {
-	var hasHelper = {};
-	for (var i = 0; i < results.length; i++) {
-		var currHelpers = results[i].babel.metadata.usedHelpers;
-		for (var j = 0; j < currHelpers.length; j++) {
-			hasHelper[currHelpers[j]] = true;
-		}
-	}
-	return Object.keys(hasHelper);
+  var hasHelper = {};
+  for (var i = 0; i < results.length; i++) {
+    var currHelpers = results[i].babel.metadata.usedHelpers;
+    for (var j = 0; j < currHelpers.length; j++) {
+      hasHelper[currHelpers[j]] = true;
+    }
+  }
+  return Object.keys(hasHelper);
 }
 
 function initializeGlobalVar(concat, options) {
-	if (!options.skipGlobalVarInit) {
-		concat.add('init.js', 'this.' + options.babelOptions._globalName + ' = {};');
-		concat.add('initNamed.js', 'this.' + options.babelOptions._globalName + 'Named = {};');
-	}
+  if (!options.skipGlobalVarInit) {
+    concat.add('init.js', 'this.' + options.babelOptions._globalName + ' = {};');
+    concat.add('initNamed.js', 'this.' + options.babelOptions._globalName + 'Named = {};');
+  }
 }
 
 function normalizeOptions(options) {
-	options = options || {};
-	options.babelOptions = options.babelOptions || {};
-	options.babelOptions.externalHelpers = true;
-	options.babelOptions.metadataUsedHelpers = true;
-	if (options.babelOptions.resolveModuleSource) {
-		var originalFn = options.babelOptions.resolveModuleSource;
-		options.babelOptions.resolveModuleSource = function(source, filename) {
-			return resolveModuleSource(originalFn(source, filename), filename);
-		};
-	} else {
-		options.babelOptions.resolveModuleSource = resolveModuleSource;
-	}
-	options.babelOptions.modules = BabelGlobalsFormatter;
-	options.babelOptions._globalName = options.globalName || 'myGlobals';
+  options = options || {};
+  options.babelOptions = options.babelOptions || {};
+  options.babelOptions.externalHelpers = true;
+  options.babelOptions.metadataUsedHelpers = true;
+  if (options.babelOptions.resolveModuleSource) {
+    var originalFn = options.babelOptions.resolveModuleSource;
+    options.babelOptions.resolveModuleSource = function(source, filename) {
+      return resolveModuleSource(originalFn(source, filename), filename);
+    };
+  } else {
+    options.babelOptions.resolveModuleSource = resolveModuleSource;
+  }
+  options.babelOptions.modules = BabelGlobalsFormatter;
+  options.babelOptions._globalName = options.globalName || 'myGlobals';
 
-	options.bundleFileName = options.bundleFileName || 'bundle.js';
-	return options;
+  options.bundleFileName = options.bundleFileName || 'bundle.js';
+  return options;
 }
 
 var dependencies = {};
 var visited = {};
 function resolveModuleSource(source, filename) {
-	dependencies[filename] = dependencies[filename] || [];
-	dependencies[filename].push(babelDeps.getFullPath(source, filename));
-	return source;
+  dependencies[filename] = dependencies[filename] || [];
+  dependencies[filename].push(babelDeps.getFullPath(source, filename));
+  return source;
 }
 
 function sortModules(results) {
-	var orderedResultPaths = [];
-	var pathToResults = {};
-	for (var i = 0; i < results.length; i++) {
-		pathToResults[results[i].path] = results[i];
-		visit(results[i].path, orderedResultPaths);
-	}
+  var orderedResultPaths = [];
+  var pathToResults = {};
+  for (var i = 0; i < results.length; i++) {
+    pathToResults[results[i].path] = results[i];
+    visit(results[i].path, orderedResultPaths);
+  }
 
-	var orderedResults = [];
-	for (var j = 0; j < orderedResultPaths.length; j++) {
-		orderedResults.push(pathToResults[orderedResultPaths[j]]);
-	}
-	return orderedResults;
+  var orderedResults = [];
+  for (var j = 0; j < orderedResultPaths.length; j++) {
+    orderedResults.push(pathToResults[orderedResultPaths[j]]);
+  }
+  return orderedResults;
 }
 
 function visit(filePath, orderedArr) {
-	if (visited[filePath]) {
-		return;
-	}
-	visited[filePath] = true;
+  if (visited[filePath]) {
+    return;
+  }
+  visited[filePath] = true;
 
-	var deps = dependencies[filePath] || [];
-	deps.forEach(function(dep) {
-		visit(dep, orderedArr);
-	});
-	orderedArr.push(filePath);
+  var deps = dependencies[filePath] || [];
+  deps.forEach(function(dep) {
+    visit(dep, orderedArr);
+  });
+  orderedArr.push(filePath);
 }
 
 module.exports = compileToGlobals;
--- test/test.js
+++ test/test.js
@@ -6,112 +6,112 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = {
-	testBuildGlobals: function(test) {
-		var files = [getFile(path.resolve('test/assets/main.js'))];
-		var result = babelGlobals(files);
-		assert.ok(result);
+  testBuildGlobals: function(test) {
+    var files = [getFile(path.resolve('test/assets/main.js'))];
+    var result = babelGlobals(files);
+    assert.ok(result);
 
-		eval(result.content.toString()); // jshint ignore:line
-		assert.ok(this.myGlobals.main);
-		assert.strictEqual('foo bar', this.myGlobals.main);
+    eval(result.content.toString()); // jshint ignore:line
+    assert.ok(this.myGlobals.main);
+    assert.strictEqual('foo bar', this.myGlobals.main);
 
-		assert.ok(result.sourceMap);
-		assert.strictEqual('bundle.js', JSON.parse(result.sourceMap).file);
+    assert.ok(result.sourceMap);
+    assert.strictEqual('bundle.js', JSON.parse(result.sourceMap).file);
 
-		test.done();
-	},
+    test.done();
+  },
 
-	testBuildGlobalsAlias: function(test) {
-		var files = [getFile(path.resolve('test/assets/mainAlias.js'))];
-		var result = babelGlobals(files, {
-			babelOptions: {
-				resolveModuleSource: function(source) {
-					if (source[0] !== '.') {
-						source = './' + source;
-					}
-					return source;
-				}
-			}
-		});
-		assert.ok(result);
+  testBuildGlobalsAlias: function(test) {
+    var files = [getFile(path.resolve('test/assets/mainAlias.js'))];
+    var result = babelGlobals(files, {
+      babelOptions: {
+        resolveModuleSource: function(source) {
+          if (source[0] !== '.') {
+            source = './' + source;
+          }
+          return source;
+        }
+      }
+    });
+    assert.ok(result);
 
-		eval(result.content.toString()); // jshint ignore:line
-		assert.ok(this.myGlobals.mainAlias);
-		assert.strictEqual('foo bar', this.myGlobals.mainAlias);
+    eval(result.content.toString()); // jshint ignore:line
+    assert.ok(this.myGlobals.mainAlias);
+    assert.strictEqual('foo bar', this.myGlobals.mainAlias);
 
-		assert.ok(result.sourceMap);
-		assert.strictEqual('bundle.js', JSON.parse(result.sourceMap).file);
+    assert.ok(result.sourceMap);
+    assert.strictEqual('bundle.js', JSON.parse(result.sourceMap).file);
 
-		test.done();
-	},
+    test.done();
+  },
 
-	testGlobalVariableInit: function(test) {
-		var files = [getFile(path.resolve('test/assets/main.js'))];
-		var result = babelGlobals(files);
-		assert.ok(result);
+  testGlobalVariableInit: function(test) {
+    var files = [getFile(path.resolve('test/assets/main.js'))];
+    var result = babelGlobals(files);
+    assert.ok(result);
 
-		var myGlobals = {};
-		var myGlobalsNamed = {};
-		this.myGlobals = myGlobals;
-		this.myGlobalsNamed = myGlobalsNamed;
-		eval(result.content.toString()); // jshint ignore:line
-		assert.notStrictEqual(myGlobals, this.myGlobals);
-		assert.notStrictEqual(myGlobalsNamed, this.myGlobalsNamed);
+    var myGlobals = {};
+    var myGlobalsNamed = {};
+    this.myGlobals = myGlobals;
+    this.myGlobalsNamed = myGlobalsNamed;
+    eval(result.content.toString()); // jshint ignore:line
+    assert.notStrictEqual(myGlobals, this.myGlobals);
+    assert.notStrictEqual(myGlobalsNamed, this.myGlobalsNamed);
 
-		test.done();
-	},
+    test.done();
+  },
 
-	testSkipGlobalVariableInit: function(test) {
-		var files = [getFile(path.resolve('test/assets/main.js'))];
-		var result = babelGlobals(files, {skipGlobalVarInit: true});
-		assert.ok(result);
+  testSkipGlobalVariableInit: function(test) {
+    var files = [getFile(path.resolve('test/assets/main.js'))];
+    var result = babelGlobals(files, {skipGlobalVarInit: true});
+    assert.ok(result);
 
-		var myGlobals = {};
-		var myGlobalsNamed = {};
-		this.myGlobals = myGlobals;
-		this.myGlobalsNamed = myGlobalsNamed;
-		eval(result.content.toString()); // jshint ignore:line
-		assert.strictEqual(myGlobals, this.myGlobals);
-		assert.strictEqual(myGlobalsNamed, this.myGlobalsNamed);
+    var myGlobals = {};
+    var myGlobalsNamed = {};
+    this.myGlobals = myGlobals;
+    this.myGlobalsNamed = myGlobalsNamed;
+    eval(result.content.toString()); // jshint ignore:line
+    assert.strictEqual(myGlobals, this.myGlobals);
+    assert.strictEqual(myGlobalsNamed, this.myGlobalsNamed);
 
-		test.done();
-	},
+    test.done();
+  },
 
-	testBuildGlobalsWithGlobalsName: function(test) {
-		var files = [getFile(path.resolve('test/assets/main.js'))];
-		var result = babelGlobals(files, {globalName: 'foo'});
-		assert.ok(result);
+  testBuildGlobalsWithGlobalsName: function(test) {
+    var files = [getFile(path.resolve('test/assets/main.js'))];
+    var result = babelGlobals(files, {globalName: 'foo'});
+    assert.ok(result);
 
-		eval(result.content.toString()); // jshint ignore:line
-		assert.ok(this.foo.main);
-		assert.strictEqual('foo bar', this.foo.main);
+    eval(result.content.toString()); // jshint ignore:line
+    assert.ok(this.foo.main);
+    assert.strictEqual('foo bar', this.foo.main);
 
-		assert.ok(result.sourceMap);
-		assert.strictEqual('bundle.js', JSON.parse(result.sourceMap).file);
+    assert.ok(result.sourceMap);
+    assert.strictEqual('bundle.js', JSON.parse(result.sourceMap).file);
 
-		test.done();
-	},
+    test.done();
+  },
 
-	testBuildGlobalsWithBundleName: function(test) {
-		var files = [getFile(path.resolve('test/assets/main.js'))];
-		var result = babelGlobals(files, {bundleFileName: 'myGlobals.js'});
-		assert.ok(result);
+  testBuildGlobalsWithBundleName: function(test) {
+    var files = [getFile(path.resolve('test/assets/main.js'))];
+    var result = babelGlobals(files, {bundleFileName: 'myGlobals.js'});
+    assert.ok(result);
 
-		eval(result.content.toString()); // jshint ignore:line
-		assert.ok(this.myGlobals.main);
-		assert.strictEqual('foo bar', this.myGlobals.main);
+    eval(result.content.toString()); // jshint ignore:line
+    assert.ok(this.myGlobals.main);
+    assert.strictEqual('foo bar', this.myGlobals.main);
 
-		assert.ok(result.sourceMap);
-		assert.strictEqual('myGlobals.js', JSON.parse(result.sourceMap).file);
+    assert.ok(result.sourceMap);
+    assert.strictEqual('myGlobals.js', JSON.parse(result.sourceMap).file);
 
-		test.done();
-	}
+    test.done();
+  }
 };
 
 var filesContents = {};
 function getFile(path) {
-	if (!filesContents[path]) {
-		filesContents[path] = fs.readFileSync(path, 'utf8');
-	}
-	return {contents: filesContents[path], options: {filename: path}};
+  if (!filesContents[path]) {
+    filesContents[path] = fs.readFileSync(path, 'utf8');
+  }
+  return {contents: filesContents[path], options: {filename: path}};
 }
